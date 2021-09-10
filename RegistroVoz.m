e0b45367cd3ce3a143ab6e0d99e4abae0f137ee1
Luis Dario <dariophoskyhinojosa@gmail.com>
2021-09-09 22:32:39 -0500

    last

--- RegistroVoz.m
+++ RegistroVoz.m
@@ -5,10 +5,7 @@ p1 = "Enter you university id: A######## \n";
 pe = "User is not registered. Enter another one: \n";
 
 mat = input(p1,'s')
-% user validation pending
-%while strcmp(mat,"A01028822") == 0 | strcmp(mat,"A01274880") == 0
-    %mat = input(pe,'s')
-%end
+
 fprintf("Identification passed.\nPlease say in loud voice: Hola mundo! Esto es una prueba.\n ");
 
 q=3;
